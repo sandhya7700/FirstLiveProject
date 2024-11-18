a126ccd5e1339d868ce99d80250b9b59a66d40e9
sandhya7700 <thakursandhya7700@gmail.com>
2024-11-18 21:48:12 +0530

    we are live

---
+++ docs/server/chunk-UGOZQU2F.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a as e,b as r,c as p,d as i,e as n,l as m,n as f,o as s,r as a,x as c}from"./chunk-RMPKOU42.mjs";var d=(()=>{class o{constructor(){this.title="DevProject"}static{this.\u0275fac=function(t){return new(t||o)}}static{this.\u0275cmp=e({type:o,selectors:[["app-root"]],standalone:!0,features:[n],decls:2,vars:0,template:function(t,y){t&1&&(r(0,"h1"),i(1,"Hello World ! How are you ?"),p())}})}}return o})();var l=[];var u={providers:[c(l),s()]};var C={providers:[a()]},v=m(u,C);var x=()=>f(d,v),T=x;export{T as a};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-UGOZQU2F.mjs";import"./chunk-RMPKOU42.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
